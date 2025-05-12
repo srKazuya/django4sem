501a9b42b613bb7b6cdbb633d281262542a99b4b
srKazuya <prodeer99@gmail.com>
2025-05-12 17:13:37 +0300

    header_0.2

--- frontend/src/App/App.tsx
+++ frontend/src/App/App.tsx
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Main from '@pages/Main'
 import Header from '@components/header'
+import styles from './App.module.scss';
+
 const App = () => {
 
   return (
-    <div className='cont'>
+    <div className={styles.cont}>
       <Router>
         <Header />
         <Routes>
--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import styles from './Header.module.scss';
+import logo from '@assets/img/logo.svg'
 
 interface Subcategory {
   id: number;
@@ -17,21 +18,46 @@ const Header = () => {
   const [categories, setCategories] = useState<Category[]>([]);
 
   useEffect(() => {
-    // Запрос данных для хедера
     axios.get('http://127.0.0.1:8000/api/categories/')
       .then(response => {
-        setCategories(response.data); // Устанавливаем данные в состояние
+        setCategories(response.data);
       })
       .catch(error => {
         console.error('Ошибка при загрузке категорий:', error);
       });
   }, []);
 
+
+  const middleIndex = Math.ceil(categories.length / 2);
+  const firstHalf = categories.slice(0, middleIndex);
+  const secondHalf = categories.slice(middleIndex);
+
   return (
     <header className={`${styles.mainCont}`}>
-      <nav>
-        <ul className={styles.menu}>
-          {categories.map(category => (
+      <nav className={styles.nav}>
+        <ul className={styles.leftMenu}>
+          {firstHalf.map(category => (
+            <li key={category.id} className={styles.menuItem}>
+              {category.name}
+              {category.subcategories.length > 0 && (
+                <ul className={styles.submenu}>
+                  {category.subcategories.map(subcategory => (
+                    <li key={subcategory.id} className={styles.submenuItem}>
+                      {subcategory.name}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </li>
+          ))}
+        </ul>
+
+        <div className={styles.logoCont}>
+          <img src={logo} alt="Логотип" className={styles.logo} />
+        </div>
+
+        <ul className={styles.rightMenu}>
+          {secondHalf.map(category => (
             <li key={category.id} className={styles.menuItem}>
               {category.name}
               {category.subcategories.length > 0 && (
